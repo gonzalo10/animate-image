baa942b7bd90c77a97868088069ce33e4b92f784
gonzalo10 <gonzalo.achiaga@gmail.com>
2020-02-09 16:46:19 +0100

    upload image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
 import React from 'react';
-import logo from './logo.svg';
+import Buttons from './Buttons';
+import { API_URL } from './config';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			file: null,
+		};
+		this.handleChange = this.handleChange.bind(this);
+		this.uploadImage = this.uploadImage.bind(this);
+	}
 
+	uploadImage(e) {
+		const errs = [];
+		const files = Array.from(e.target.files);
+
+		const formData = new FormData();
+		const types = ['image/png', 'image/jpeg', 'image/gif'];
+
+		files.forEach((file, i) => {
+			if (types.every(type => file.type !== type)) {
+				errs.push(`'${file.type}' is not a supported format`);
+			}
+
+			if (file.size > 150000) {
+				errs.push(`'${file.name}' is too large, please pick a smaller file`);
+			}
+
+			formData.append(i, file);
+		});
+
+		if (errs.length) {
+			throw errs;
+		}
+
+		fetch(`${API_URL}/image-upload`, {
+			method: 'POST',
+			body: formData,
+		})
+			.then(res => {
+				if (!res.ok) {
+					throw res;
+				}
+				return res.json();
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
+
+	handleChange(event) {
+		this.uploadImage(event);
+		this.setState({
+			file: URL.createObjectURL(event.target.files[0]),
+		});
+	}
+	render() {
+		return (
+			<div>
+				<Buttons onChange={this.handleChange} />
+				<img width={600} src={this.state.file} />
+			</div>
+		);
+	}
+}
 export default App;
